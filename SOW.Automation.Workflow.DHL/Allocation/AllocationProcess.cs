d56f68536972d97fddf194b8a3abd3457f15f40d
rafael sanchez port <rafysanchez@hotmail.com>
2019-04-18 16:59:56 -0300

    adjs

--- SOW.Automation.Workflow.DHL/Allocation/AllocationProcess.cs
+++ SOW.Automation.Workflow.DHL/Allocation/AllocationProcess.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SOW.Automation.Interface.DLx.Windows.TaskManagementDetailed;
 using SOW.Automation.Interface.DLx.Excel;
 using SOW.Automation.Interface.DLx.Excel.Models;
 using SOW.Automation.Interface.DLx.Excel.Services;
+using SOW.Automation.Interface.DLx.Models;
+
 namespace SOW.Automation.Workflow.DHL.Allocation
 {
 	public static class AllocationProcess
@@ -116,13 +119,13 @@ namespace SOW.Automation.Workflow.DHL.Allocation
 							string path = System.String.Concat(System.Environment.CurrentDirectory, @"\Files", @"\");
 							string file = @"Planilha de Testes.xlsm";
 							
-							var taskManagementDetailed = new SOW.Automation.Interface.DLx.Windows.TaskManagementDetailed.TaskManagementDetailed1920x1080(services.WebService, services.DesktopService, services.OCRService);
-							System.Collections.Generic.IList<Interface.DLx.Models.ShipmentDetailed> ShipmentDetaileds = taskManagementDetailed.ModelarItensSelecionados().Where(x => x != null && x.StatusMS != null && x.StatusMS.ToLower().Contains("ready")).ToList();
+							var taskManagementDetailed = new TaskManagementDetailed1920x1080(services.WebService, services.DesktopService, services.OCRService);
+							IList<ShipmentDetailed> ShipmentDetaileds = taskManagementDetailed.ModelarItensSelecionados().Where(x => x != null && x.StatusMS != null && x.StatusMS.ToLower().Contains("ready")).ToList();
 							foreach (var shipment in ShipmentDetaileds.Take(5)) {
 								double boxes = ((shipment.TotalCaixas != null && !System.String.IsNullOrWhiteSpace(shipment.TotalCaixas)) ? int.Parse(shipment.TotalCaixas) : 1);
 								double pallets = ((shipment.TotalPallets != null && !System.String.IsNullOrWhiteSpace(shipment.TotalPallets)) ? int.Parse(shipment.TotalPallets) : 1);
 								double special = ((!System.String.IsNullOrWhiteSpace(shipment.PadraoEspecial)) ? 1.3 : 1);
-								var rpa = new Interface.DLx.Excel.Models.RpaDadosStageInfo {
+								var rpa = new RpaDadosStageInfo {
 									Ms = shipment.MS,
 									Sid = shipment.ShipmentID,
 									Status = Interface.DLx.Excel.Enums.EStatusStage.Ready,
